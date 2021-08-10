06d0eb04f1daab79032d8f07f704fe41140c0c52
devenshettyss <practice.devenshetty@gmail.com>
2021-08-10 10:18:19 +0000

    VarArgsDemo

---
+++ src/com/tcs/entity/VarArgsDemo.java
@@ -0,0 +1,23 @@
+package com.tcs.entity;
+
+public class VarArgsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] subjects = {"Math", "English"};
+		Student student = new Student(subjects);
+		System.out.println(student.subjects[0]);
+		
+		Student student1 = new Student("Science");
+		System.out.println(student1.subjects[0]);
+
+	}
+
+}
+
+class Student{
+	String[] subjects;
+	public Student(String... subjects) {
+		this.subjects=subjects;
+	}
+}
