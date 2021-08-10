a0c4d8aac42ee044b1e347a21d67392a9092ac4c
devenshettyss <practice.devenshetty@gmail.com>
2021-08-10 10:07:52 +0000

    Enhanced For Loop

--- src/com/tcs/entity/ArraysDemo.java
+++ src/com/tcs/entity/ArraysDemo.java
@@ -1,15 +1,30 @@
 package com.tcs.entity;
+import java.util.Arrays;
 
 public class ArraysDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		EnhancedForLoopDemo();
+		}
+	private static void EnhancedForLoopDemo() {
+		// TODO Auto-generated method stub
+		int[] numbers = {3,4,-1,2,6};
+		for(int number : numbers) {
+			if(number % 2 == 0) {
+				System.out.println(number);
+			}
+		}
+	}
+	
+	
+	private static void demo1() {
 		int[] numbers = new int[10];
 		numbers[0]= 3;
 		numbers[4]= 1;
-		for(int i=0; i<numbers.length;i++) {
-			System.out.println(numbers[i]);
-		}
+		for(int counter=0; counter<numbers.length;counter++) {
+			System.out.println(numbers[counter]);
 	}
 
-}
+	}
+	}
