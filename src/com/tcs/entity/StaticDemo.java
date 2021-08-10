8f128601f3f557aabd1cc4523c2803125eef0d1f
devenshettyss <practice.devenshetty@gmail.com>
2021-08-10 09:06:27 +0000

    static, enum

---
+++ src/com/tcs/entity/StaticDemo.java
@@ -0,0 +1,21 @@
+package com.tcs.entity;
+
+public class StaticDemo {
+	public static void main(String[] args) {
+
+		Bank bank = new Bank();
+		System.out.println(Bank.createNewAccountNumber());	
+		System.out.println(Bank.createNewAccountNumber());
+	
+	}
+
+	
+}
+
+class Bank{
+	static int counter;
+	int instanceVariable=10;
+	public static int createNewAccountNumber() {
+		return ++counter;
+	}
+}
