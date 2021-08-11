0b58738d89043708ad46e111da7438a5b3a25003
devenshettyss <practice.devenshetty@gmail.com>
2021-08-11 06:29:07 +0000

    static update

--- src/com/tcs/statickeyword/StaticBlockDemo.java
+++ src/com/tcs/statickeyword/StaticBlockDemo.java
@@ -2,10 +2,12 @@ package com.tcs.statickeyword;
 
 public class StaticBlockDemo {
 	private static int age=10;
+	int price=20;
 	public StaticBlockDemo() {
 		System.out.println("in constructor..");
 	}
 	static {
+		final int localVariable=40;
 		System.out.println("This is static block."+age);
 	}
 	public static void main(String[] args) {
