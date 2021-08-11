ba4a1689cbf88178f1a1337b8bb887ca603bbacc
devenshettyss <practice.devenshetty@gmail.com>
2021-08-11 06:40:47 +0000

    static

---
+++ src/com/tcs/practice/FileReader.java
@@ -0,0 +1,10 @@
+package com.tcs.practice;
+
+import java.util.Arrays;
+
+public class FileReader {
+	public static void main(String[] args) {
+	int[] numbers = {4,5,6,7,9};
+	Arrays.sort(numbers);
+	}
+}
