c1738f7b994b35a5d0473681b8e635c3c9e01dea
devenshettyss <practice.devenshetty@gmail.com>
2021-08-11 10:59:50 +0000

    Multi threading

--- src/com/tcs/threads/Worker.java
+++ src/com/tcs/threads/Worker.java
@@ -2,10 +2,14 @@ package com.tcs.threads;
 
 public class Worker implements Runnable{
 
-	
-		@Override
-		public void run(){
+	@Override
+	public void run() {
 		System.out.println(Thread.currentThread());
+		try {
+			Thread.sleep(99999);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
