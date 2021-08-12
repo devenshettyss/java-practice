b875ac6b1b3a5cc188ad1a2ff372568c63a20c1b
devenshettyss <practice.devenshetty@gmail.com>
2021-08-12 07:23:49 +0000

    TreeSet updated

--- src/com/tcs/set/examples/User.java
+++ src/com/tcs/set/examples/User.java
@@ -1,6 +1,8 @@
 package com.tcs.set.examples;
 
-public class User {
+import java.util.Comparator;
+
+public class User implements Comparable<User>  {
 private String name;
 private int age;
 public String getName() {
@@ -18,5 +20,9 @@ public void setAge(int age) {
 @Override
 public String toString() {
 	return Integer.toString(age);
+}
+@Override
+public int compareTo(User user) {
+	return user.getAge() - this.getAge() ;
 }
 }
